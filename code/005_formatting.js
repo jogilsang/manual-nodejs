2108704098d79ae9825b28230daf005425de7eef
jogilsang <jogilsang@gmail.com>
2021-02-20 15:45:15 +0900

    added : 005_formatting.js

---
+++ code/005_formatting.js
@@ -0,0 +1,10 @@
+
+
+
+console.log('name:%s', 'kyeongrok'); // 템플릿 형식 출력
+console.log('age:%d', 31) // 숫자는 %d
+console.log('pi:%d', 3.14) // 숫자는 %d
+console.log('math:%d science:%d', 92,84); // 2가지 같이쓰기
+console.log('name:%s age:%d', 'gilsang', 31); //여러가지 같이쓰기
+console.log('name:%s age:%s', 'gilsang', 31); // %s는 다 표시가능
+
