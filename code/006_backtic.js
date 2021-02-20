7b2011e76ba8cbe01b5cd1a063cf10687753a515
jogilsang <jogilsang@gmail.com>
2021-02-20 16:02:17 +0900

    added : 006_backtic.js

---
+++ code/006_backtic.js
@@ -0,0 +1,13 @@
+
+
+
+const greeting1 = 'hello';
+const greeting2 = 'hello';
+
+const name1 = 'gilsang';
+const name2 = 'jain';
+
+const statement = `${name1} + is awesome!`;
+
+console.log(`${name1} + is awesome!`);
+console.log(statement);
