016d96caeff806e9ee25b859ce05d3c3a67be74a
jogilsang <jogilsang@gmail.com>
2021-02-22 02:58:22 +0900

    added : 034_forof.js

---
+++ code/034_forof.js
@@ -0,0 +1,16 @@
+
+const userList = [
+
+    { name : 'gilsang', age : 31, score : 85},
+    { name : 'gilsang2', age : 32, score : 86},
+    { name : 'gilsang3', age : 33, score : 87}
+
+];
+
+for (var user of userList) {
+    console.log('user :', user);
+}
+
+// user : { name: 'gilsang', age: 31, score: 85 }
+// user : { name: 'gilsang2', age: 32, score: 86 }
+// user : { name: 'gilsang3', age: 33, score: 87 }
