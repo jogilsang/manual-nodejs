4a697de8a0c672dc290de09949c91da554100f4f
jogilsang <jogilsang@gmail.com>
2021-02-21 10:06:35 +0900

    added : 021_typeof.js

---
+++ code/021_typeof.js
@@ -0,0 +1,18 @@
+
+
+
+const pi = 3.14;
+const person = 'gilsang';
+const arr = [];
+const json = {};
+
+console.log(typeof(pi));
+console.log(typeof(person));
+console.log(typeof(arr));
+console.log(typeof(json));
+
+// response
+// number
+// string
+// object
+// object
