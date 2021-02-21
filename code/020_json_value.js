060715959aae2a19595b33d14f031bb1ef64853d
jogilsang <jogilsang@gmail.com>
2021-02-21 10:00:29 +0900

    added : 020_json_value

---
+++ code/020_json_value.js
@@ -0,0 +1,18 @@
+
+
+const user = {
+
+    name : 'gilsang',
+    age : 31
+};
+
+user.job = 'person';
+user.nation = 'korea';
+
+console.log(user.job);
+console.log(user['job']);
+
+const fieldName = 'nation';
+
+console.log(user.nation);
+console.log(user[fieldName]);
