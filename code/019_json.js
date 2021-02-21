3a70a0b402a144d0372216425ca35690e760b31a
jogilsang <jogilsang@gmail.com>
2021-02-21 09:56:51 +0900

    added : 019_json.js

---
+++ code/019_json.js
@@ -0,0 +1,20 @@
+
+
+
+const user = {};
+
+user.name = 'gilsang';
+user.age = 31;
+
+console.log(user);
+
+const user2 = {
+    name :'gilsang',
+    age : 31
+};
+
+console.log(user2);
+
+// response
+// { name: 'gilsang', age: 31 }
+// { name: 'gilsang', age: 31 }
