511a235648d00cfdf3729c0d60d9e909c8f50b26
jogilsang <jogilsang@gmail.com>
2021-02-23 05:42:05 +0900

    modifyed : 037_date.js

--- code/037_date.js
+++ code/037_date.js
@@ -37,4 +37,15 @@ console.log('hours : %s', date.getHours()); 18
 console.log('minutes : %s', date.getMinutes()); 20
 console.log('seconds : %s', date.getSeconds()); 30
 
+// yyyy-MM-dd 형식으로 날짜 출력하기
+const getYymmdd = (date) => {
+    const yyyy = date.getFullYear();
+    const mm = date.getMonth() < 9 ? `0${date.getMonth() +1}` : (date.getMonth() + 1);
+    const dd = date.getDate() < 10 ? `0${date.getDate}` : date.getDate();
+    return `${yyyy}-${mm}-${dd}`;
+};
 
+const yymmdd = getYymmdd(new Date());
+const yymmdd170909 = getYymmdd(new Date(2017,10-1, 10));
+console.log(yymmdd); // 2021-02-23
+console.log(yymmdd170909); // 2017-10-10
