fd95c9300ee7d6b6b59f0a98b8790c17454b6e4c
jogilsang <jogilsang@gmail.com>
2021-02-27 09:47:24 +0900

    added : 043_globalobject.js

---
+++ code/044_function.js
@@ -0,0 +1,49 @@
+
+
+
+// 044. function
+// 함수란 ?
+// 프로그래밍에서 명령문을 묶은 단위
+
+function printHello () {
+    console.log('hello');
+}
+printHello();
+
+// ES6
+const printBye = () => {
+    console.log('bye');
+}
+printBye();
+
+// 결과
+// hello
+// bye
+
+// 045. 매개변수(parameter), 인자(argument)
+// 매개변수 ? : 값이 넘어오는 매개체
+// 인자 ? : argument
+function printMessage(pMessage) {
+
+}
+
+// 048. 함수 선언식(Function Declaration), 함수 표현식(Funtion Expression)
+
+// 049. 함수의 성질
+
+function plus(a,b) {
+    return a+b;
+}
+
+function calculate(a,b, func) {
+    return func(a,b);
+}
+
+console.log(calculate(3,5,plus));
+
+// 결과
+// 8
+
+// 050. arrow 함수
+
+const print
