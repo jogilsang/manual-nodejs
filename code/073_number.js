e1ac17b79a6d03e46a614f15fe972200217435df
jogilsang <jogilsang@gmail.com>
2021-03-01 05:34:54 +0900

    added : 073_number.js

---
+++ code/073_number.js
@@ -0,0 +1,13 @@
+
+
+
+//073_parseint_number
+
+const num = 175.5;
+
+console.log(`parseInt(num) = ${parseInt(num)}`);
+console.log(`Number(num) = ${Number(num)}`);
+
+// // print
+// parseInt(num) = 175
+// Number(num) = 175.5
