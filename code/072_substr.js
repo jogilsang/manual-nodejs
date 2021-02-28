29d837fc925cbeeb8524abd07ce84dc330e900e0
jogilsang <jogilsang@gmail.com>
2021-03-01 05:34:38 +0900

    aded : 072_substr.js

---
+++ code/072_substr.js
@@ -0,0 +1,13 @@
+
+
+//072_substring
+
+const string = 'abcdfwefwefwefsdfsdfsdsfdz';
+
+console.log(`string.substring(0, string.length) = ${string.substring(0, string.length)}`);
+console.log(`string.substring(10, string.length) = ${string.substring(10, string.length)}`);
+
+
+// print
+// string.substring(0, string.length) = abcdfwefwefwefsdfsdfsdsfdz
+// string.substring(10, string.length) = fwefsdfsdfsdsfdz
