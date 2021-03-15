ef01f981e33759986d1ed13f3b7a26e34cfb9298
jogilsang <jogilsang@gmail.com>
2021-03-16 04:17:10 +0900

    added : 143_nodejs 모듈설명

---
+++ code/143_nodejs.js
@@ -0,0 +1,17 @@
+
+// npm init : package.json 만들기
+// npm install package --save : --save 옵션은 package.json에 등록
+// npm install package --save-dev : devDependencies
+
+// request : 서버에 요청을 보낼 때, request 모듈을 이용
+// cheerio : jquery 문법 지원 웹 페이지에서 데이터를 추출
+// iconv-lite : 컴파일이 없는 순수한 자바스크립트 코드. 다양한 인코딩 제공
+// ejs : 템플릿 엔진 모듈로 특정 형식의 문자열을 HTML 형식의 문자열로 변환해준다.
+// winston : 로그남기기
+// node-schedule : batch 작업
+
+// 미들웨어
+// body-parser : POST 요청을 처리할 때 사용자가 보낸 데이터를 추출
+// router : 라우팅을 지원하는 라이브러리. 라우팅은 URI가 클라이언트 요청에 응답하는 방식. URI는 어플리케이션의 앤드포인트
+// morgan : 로깅
+// cookie-parser : 
